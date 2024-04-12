d05c3af57c1e63301b17f7652d693365bb2ede29
LUCCA ALVES DA SILVA <lucca_a_silva@estudante.sesisenai.org.br>
2024-04-11 21:51:36 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, Touchable, View, TouchableOpacity } from 'react-native';
+import BotaoCustomizado from './comum/componentes/BotaoCustomizado/BotaoCustomizado';
+import CORES from './comum/constantes/cores';
+
+const estilos = StyleSheet.create({
+  tudo: {
+    //flex 1 faz preencher a tela inteira
+    flex: 1,
+    // backgroundColor: '#ffdb58',
+    backgroundColor: CORES.FUNDO_PADRAO,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    gap: 16,
+  },
+  contador: {
+    fontSize: 72,
+    color: CORES.TEXTO_PADRAO
+  },
+  botao: {
+    backgroundColor: 'blue',
+    width: 64,
+    height: 64,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  botaoTexto: {
+    fontSize: 48,
+    color: '#fff',
+  }
+});
 
 export default function App() {
+  const [contador, setContador] = React.useState(0);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+
+    <View style={estilos.tudo}>
+
+      {/* <BotaoCustomizado /> */}
+
+      <BotaoCustomizado cor='primaria' onPress={() => setContador(contador - 1)}>
+        -
+      </BotaoCustomizado>
+
+      <Text style={estilos.contador}>{contador}</Text>
+
+      <BotaoCustomizado cor='secundaria' onPress={() => setContador(contador + 1)}>
+        +
+      </BotaoCustomizado>
+
+      {/* <StatusBar estilos={botaoTexto} style="auto" /> */}
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
