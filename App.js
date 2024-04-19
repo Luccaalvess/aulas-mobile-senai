d87c32bbb77e94fccdcf3e705c143276fa6a934a
LUCCA ALVES DA SILVA <lucca_a_silva@estudante.sesisenai.org.br>
2024-04-18 22:05:43 -0300

    reestruturação para o uso do React Navigation

--- App.js
+++ App.js
@@ -1,60 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, Touchable, View, TouchableOpacity } from 'react-native';
-import BotaoCustomizado from './comum/componentes/BotaoCustomizado/BotaoCustomizado';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import CORES from './comum/constantes/cores';
+import TelaContador from './telas/TelaContador/TelaContador';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import TelaPrincipal from './telas/TelaPrincipal/TelaPrincipal';
+import TELAS from './comum/constantes/telas';
+
+const Stack = createStackNavigator();
 
 const estilos = StyleSheet.create({
-  tudo: {
+  todoApp: {
     //flex 1 faz preencher a tela inteira
     flex: 1,
-    // backgroundColor: '#ffdb58',
     backgroundColor: CORES.FUNDO_PADRAO,
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'row',
-    gap: 16,
   },
-  contador: {
-    fontSize: 72,
-    color: CORES.TEXTO_PADRAO
-  },
-  botao: {
-    backgroundColor: 'blue',
-    width: 64,
-    height: 64,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  botaoTexto: {
-    fontSize: 48,
-    color: '#fff',
-  }
 });
 
 export default function App() {
-  const [contador, setContador] = React.useState(0);
-
   return (
+    <View style={estilos.todoApp}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name={TELAS.TELA_PRINCIPAL} component={TelaPrincipal} />
+          <Stack.Screen name={TELAS.TELA_CONTADOR} component={TelaContador} />
+        </Stack.Navigator>
+      </NavigationContainer>     
+      {/* <TelaContador /> */}
 
-
-    <View style={estilos.tudo}>
-
-      {/* <BotaoCustomizado /> */}
-
-      <BotaoCustomizado cor='primaria' onPress={() => setContador(contador - 1)}>
-        -
-      </BotaoCustomizado>
-
-      <Text style={estilos.contador}>{contador}</Text>
-
-      <BotaoCustomizado cor='secundaria' onPress={() => setContador(contador + 1)}>
-        +
-      </BotaoCustomizado>
-
-      {/* <StatusBar estilos={botaoTexto} style="auto" /> */}
+      <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 
